6e4e37cbb1130c519ca26f5289204f3195fc53f5
Anika Kamath <87904385+anika-kamath@users.noreply.github.com>
2023-02-02 09:40:35 +0000

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)  Changes to be committed: 	new file:   sjf_nonpreemptive.cpp

---
+++ sjf_nonpreemptive.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//structure for every process
+struct Process {
+   int pid; // Process ID
+   int bt; // Burst Time
+   int art; // Arrival Time
+};
+
